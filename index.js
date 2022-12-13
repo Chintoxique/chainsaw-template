d229f934f5319a30ad4f1885c8d64b94b10b21f8
nan[0] <56279981+nonobstant@users.noreply.github.com>
2022-12-13 13:22:37 +0100

    init express

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const Express = require("express")();
+const Http = require("http").Server(Express);
+
 const request = require('request');
 const baseUrl = 'https://soccer.sportmonks.com/api/v2.0';
 const apiToken = 'your-api-token';
@@ -14,4 +17,8 @@ const livescores = JSON.parse(body).data;
 livescores.forEach(livescore => {
 console.log(livescore);
 });
+});
+
+Http.listen(process.env.PORT || 3000, () => {
+  console.log("Listening...");
 });
